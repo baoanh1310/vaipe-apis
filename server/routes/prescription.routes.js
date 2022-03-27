8756b516e9b3979fc166924eb392043feaac35d9
baoanh1310 <dataholicguy@gmail.com>
2022-03-28 01:22:30 +0700

    upload image for prescription API

--- server/routes/prescription.routes.js
+++ server/routes/prescription.routes.js
@@ -2,12 +2,13 @@ import express from 'express'
 import userCtrl from '../controllers/user.controller'
 import authCtrl from '../controllers/auth.controller'
 import prescriptionCtrl from '../controllers/prescription.controller'
+import upload from '../libs/multer'
 
 const router = express.Router()
 
 router.route('/api/prescription/:userId')
   .get(authCtrl.requireSignin, authCtrl.hasAuthorization, prescriptionCtrl.getStatsPrescription)
-  .post(authCtrl.requireSignin, authCtrl.hasAuthorization, prescriptionCtrl.create)
+  .post(authCtrl.requireSignin, authCtrl.hasAuthorization, upload.single('image'), prescriptionCtrl.create)
 
 router.param('userId', userCtrl.userByID)
 
